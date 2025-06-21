1f87b617f5cfcc78185196743e2091dbdbfa2f30
Vinayak Varshney <vinayak.varshney.dev@gmail.com>
2025-06-22 02:22:45 +0530

    Initial Commit

---
+++ backend.py
@@ -0,0 +1,55 @@
+import mysql.connector as ms
+import pandas as pd
+
+
+
+def sql_to_df(table_name,_cursor):
+    
+    _cursor.execute(f"SELECT * FROM {table_name}")
+    result = _cursor.fetchall()
+
+    df = pd.DataFrame(result)
+    
+    return df
+
+def load_data(db_name):
+    global cursor
+    conn = ms.connect(
+        host="localhost",     
+        user="root",          
+        password="vini", 
+        database=db_name
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SHOW TABLES;")
+    tables = cursor.fetchall()
+    dataframes = {}
+    for i in tables:
+        table_name = i[0]
+        dataframes[table_name] = sql_to_df(table_name,cursor)
+
+    return dataframes
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
