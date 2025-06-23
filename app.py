e7c9d6e93babc7a970e778df49395dec4fa9b6a7
Vinayak Varshney <vinayak.varshney.dev@gmail.com>
2025-06-24 00:27:20 +0530

    Added shitty user auth and updated backend logic

--- app.py
+++ app.py
@@ -1,1 +1,51 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+from backend import *
+
+st.set_page_config(page_title="Home", page_icon="🏠")
+
+
+# Dropdown menu
+menu = ["Home", "Products"]
+choice = st.sidebar.selectbox("Menu", menu)
+auth = False
+if "show_create_form" not in st.session_state:
+    st.session_state.show_create_form = False
+if "show_login_form" not in st.session_state:
+    st.session_state.show_login_form = False
+if choice == "Home":
+    st.title("🏠 Home")
+    if st.button("Login",key="login_form"):
+        st.session_state.show_login_form = True
+        
+    if st.session_state.show_login_form:
+        usr =st.text_input("Username",key="login_username")
+        pswd = st.text_input("Password",key="login_password")
+        if st.button("Login",key="login_button"):
+            if authenticate_user(usr,pswd):
+                auth = True
+                st.success("Successfully Logged in")
+            else:
+                st.error("Username or password incorrect")
+
+    if st.button("Create New Account"):
+        st.session_state.show_create_form = True
+   
+    if st.session_state.show_create_form:
+        new_usr =st.text_input("Username",key="create_username")
+        new_pswd = st.text_input("Password",key="create_password",type="password")
+        new_email = st.text_input("Email",key="create_email")
+        if st.button("Create Account"):
+            if create_user(new_usr,new_pswd,new_email):
+                st.success("Successfully Created Account")
+            else:
+                st.error("ERR-01-account with email or phone number already exists")
+
+
+elif choice == "Products":
+    st.title("Products")
+
+
+
+
+
--- backend.py
+++ backend.py
@@ -27,7 +27,7 @@ def sql_to_df(table_name):
 
 
 
-def load_data():
+def load_all_data():
     conn = get_connection("retail_store")
     cursor = conn.cursor()
     cursor.execute("SHOW TABLES;")
@@ -39,14 +39,57 @@ def load_data():
 
     return dataframes
 
+def get_all_products():
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products")
+    result = cursor.fetchall()
+    columns = cursor.column_names
+    products = pd.DataFrame(result)
+    products.columns = columns
+    
+    return products
 
-def add_categories(cursor,cat_name):
+def update_product(product_id, name, category, price, quantity):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE products SET name={name}, category={category}, price={price}, quantity={quantity} WHERE id={product_id}")
+    conn.commit()
 
-    return 
 
+def create_product(name, category, price, quantity):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO products (name, category, price, quantity) VALUES ({name}, {category}, {price}, {quantity})")
+    conn.commit()
 
+def delete_product(product_id):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM products WHERE id=%s", (product_id,))
+    conn.commit()
 
+def authenticate_user(username, password):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    result = cursor.fetchone()
+    if result:
+        return True
+    else:
+        return False
 
+def create_user(username,password,email):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username,password,email) VALUES (%s,%s,%s)",(username,password,email))
+    conn.commit()
+    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    verify = cursor.fetchone()
+    if verify:
+        return True
+    else:
+        return False
 
     
 
