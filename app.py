cae0646313cde0d651fd0cf7c8b2f460df978f82
Vinayak Varshney <vinayak.varshney.dev@gmail.com>
2025-07-02 12:18:55 +0530

    now you can add products and code is beautiful

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from backend import *
 
 st.set_page_config(page_title="Home", page_icon="🏠")
 
-
-# Dropdown menu
-menu = ["Home", "Products"]
-choice = st.sidebar.selectbox("Menu", menu)
+    
 if "auth" not in st.session_state:
         st.session_state.auth = False
 
+if "add_prod" not in st.session_state:
+        st.session_state.add_prod = False
+
+if "cat_id" not in st.session_state:
+        st.session_state.cat_id = None
+
+
 if not(st.session_state.auth):
+    st.title("Enter your credentials")
     if "show_create_form" not in st.session_state:
         st.session_state.show_create_form = False
     if "show_login_form" not in st.session_state:
@@ -19,39 +24,86 @@ if not(st.session_state.auth):
 
     if st.button("Login",key="login_form"):
         st.session_state.show_login_form = True
+        st.session_state.show_create_form = False
             
     if st.session_state.show_login_form:
-        usr =st.text_input("Username",key="login_username")
-        pswd = st.text_input("Password",key="login_password",type="password")
-        if st.button("Login",key="login_button"):
-            if authenticate_user(usr,pswd):
-                st.session_state.auth = True
-                st.success("Successfully Logged in")
-            else:
-                st.error("Username or password incorrect")
+        st.title("Login")
+        with st.form("login_form"):
+            username = st.text_input("Username")
+            st.session_state.username = username
+            password = st.text_input("Password", type="password")
+            submit_button = st.form_submit_button("Login")
+            if submit_button:
+                if authenticate_user(username, password):
+                    st.session_state.auth = True
+                    st.session_state.show_login_form = False
+                    st.success("Successfully Logged in")
+                    st.rerun()
+                else:
+                    st.error("Username or password incorrect")
 
     if st.button("Create New Account"):
-        st.session_state.show_create_form = True
+        st.session_state.show_login_form = False
+        st.session_state.show_create_form = True    
     
     if st.session_state.show_create_form:
-        new_usr =st.text_input("Username",key="create_username")
-        new_pswd = st.text_input("Password",key="create_password",type="password")
-        new_email = st.text_input("Email",key="create_email")
-        if st.button("Create Account"):
-            if create_user(new_usr,new_pswd,new_email):
-                st.success("Successfully Created Account")
-                st.session_state.show_create_form = True
-            else:
-                st.error("ERR-01-account with email or phone number already exists")
-else:
-    if choice == "Home":
-        st.title("🏠 Home")
+        st.title("Create New Account")
+        with st.form("create_form"):
+            new_usr =st.text_input("Username")
+            new_pswd = st.text_input("Password",type="password")
+            new_email = st.text_input("Email")
+            submit_button = st.form_submit_button("Create Account",)
+            if submit_button:
+                if create_user(new_usr,new_pswd,new_email):
+                    st.session_state.auth = True
+                    st.session_state.show_create_form = False
+                    st.success("Successfully Created Account")
+                    st.rerun()
+                else:
+                    st.error("ERR-01-account with email already exists")
 
-    if choice == "Products":
-        st.title("Products")
+if st.session_state.auth:
+    
+    title_placeholder = st.empty()
+
+    if st.session_state.add_prod:
+        title_placeholder.title("Add Product")
+        with st.form("add_prod_form"):    
+            p_nm = st.text_input("Product Name")
+            desc = st.text_input("Description")
+            cat_id = st.number_input("Category Id")
+            stock_qty = st.number_input("Stock Quantity")
+            price = st.number_input("Price")
+            sup_id = st.number_input("Supplier Id")
+            
+
+            submit_button = st.form_submit_button("Add Product")
+
+            if submit_button:
+                create_product(p_nm,desc,price,stock_qty,cat_id,sup_id)
+                st.success("Successfully Added Product")
+                st.session_state.add_prod = False
+                st.rerun()
+    
+    title_placeholder.title(f"🏠Welcome {st.session_state.username}")
+    df = load_all_data()
+    categories = df["categories"]
+    cat_names = st.radio("Select Category",[cat for cat in categories.category_name])
+    cat_id_dict = {}
+    for i,cat in zip(categories.category_id,categories.category_name):
+        cat_id_dict[cat] = i
+    cat_id = cat_id_dict[cat_names]
+
+    products = df["products"]
+    products = products[products.category_id == cat_id]
+    product_names = products.product_name
+    st.dataframe(products)
 
 
-
+    if st.button("Add Product"):
+        st.session_state.cat_name = cat_names
+        st.session_state.add_prod = True
+        st.rerun()
 
 
 
--- backend.py
+++ backend.py
@@ -17,7 +17,7 @@ def get_connection(db_name):
 def sql_to_df(table_name):
     conn = get_connection("retail_store")
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
+    cursor.execute(f"SELECT * FROM {table_name};")
     result = cursor.fetchall()
     columns = cursor.column_names
     df = pd.DataFrame(result)
@@ -42,7 +42,7 @@ def load_all_data():
 def get_all_products():
     conn = get_connection("retail_store")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM products")
+    cursor.execute("SELECT * FROM products;")
     result = cursor.fetchall()
     columns = cursor.column_names
     products = pd.DataFrame(result)
@@ -53,14 +53,14 @@ def get_all_products():
 def update_product(product_id, name, category, price, quantity):
     conn = get_connection("retail_store")
     cursor = conn.cursor()
-    cursor.execute(f"UPDATE products SET name={name}, category={category}, price={price}, quantity={quantity} WHERE id={product_id}")
+    cursor.execute(f"UPDATE products SET name={name}, category={category}, price={price}, quantity={quantity} WHERE id={product_id};")
     conn.commit()
 
 
-def create_product(name, category, price, quantity):
+def create_product(product_name,description,price,stock_quantity,category_id,supplier_id):
     conn = get_connection("retail_store")
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO products (name, category, price, quantity) VALUES ({name}, {category}, {price}, {quantity})")
+    cursor.execute("INSERT INTO products (product_name,description,price,stock_quantity,category_id,supplier_id) VALUES (%s,%s,%s,%s,%s,%s);",(product_name,description,price,stock_quantity,category_id,supplier_id))
     conn.commit()
 
 def delete_product(product_id):
@@ -69,10 +69,41 @@ def delete_product(product_id):
     cursor.execute("DELETE FROM products WHERE id=%s", (product_id,))
     conn.commit()
 
+def get_all_orders():
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM orders")
+    result = cursor.fetchall()
+    columns = cursor.column_names
+    orders = pd.DataFrame(result)
+    orders.columns = columns
+    
+    return orders
+
+def update_order(status,order_id):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("UPDATE orders SET status=%s WHERE id=%s", (status, order_id))
+    conn.commit()
+
+def add_order(user_id, product_id, quantity):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO orders (product_id, quantity) VALUES (%s, %s, %s);", (user_id, product_id, quantity))
+    conn.commit()
+
+def delete_order(order_id):
+    conn = get_connection("retail_store")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM orders WHERE id=%s", (order_id,))
+    conn.commit()
+
+
+
 def authenticate_user(username, password):
     conn = get_connection("retail_store")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s;", (username, password))
     result = cursor.fetchone()
     if result:
         return True
@@ -82,26 +113,11 @@ def authenticate_user(username, password):
 def create_user(username,password,email):
     conn = get_connection("retail_store")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (username,password,email) VALUES (%s,%s,%s)",(username,password,email))
+    cursor.execute("INSERT INTO users (username,password,email) VALUES (%s,%s,%s);",(username,password,email))
     conn.commit()
-    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s;", (username, password))
     verify = cursor.fetchone()
     if verify:
         return True
     else:
         return False
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
